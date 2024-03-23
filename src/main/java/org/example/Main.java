9594b6251fedf202db37691dd7dd938e36e17475
Glenn <glennedwardvilalflor@gmail.com>
2024-03-23 14:19:58 +0800

    testing discord 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello Testing123!");
+        System.out.println("Hello Testing!");
     }
 }
