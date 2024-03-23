d4f9c8a07667876743d019e3983ed2d5e32660d7
Glenn <glennedwardvilalflor@gmail.com>
2024-03-23 14:36:07 +0800

    Github to Discord

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello Testing....");
         System.out.println("Hello Discord....");
+        System.out.println("Sa wakas gumana na hahahaha....");
     }
 }
